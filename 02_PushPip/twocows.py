96da87d9fb2dc92d799f15465f24b948731793b2
Mary <maraihaidei03@gmail.com>
2025-02-24 22:36:53 +0300

    small changes

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -1,9 +1,29 @@
 import argparse
 import cowsay
-import sys
 
+parser = argparse.ArgumentParser(description='Display two cows.')
 
-parser = argparse.ArgumentParser(description = "parse second cow")
+parser.add_argument('m1', type=str, help='Message for the first cow')
+parser.add_argument('m2', type=str, help='Message for the second cow')
+
+parser.add_argument(
+    "-e",
+    type=str,
+    help="An eye string. This is ignored if a preset mode is given",
+    dest="eyes",
+    default=Option.eyes,
+    metavar="eye_string",
+)
+parser.add_argument(
+    "-f", type=str, metavar="cowfile",
+    help="Either the name of a cow specified in the COWPATH, "
+         "or a path to a cowfile (if provided as a path, the path must "
+         "contain at least one path separator)",
+)
+parser.add_argument(
+    "-n", action="store_false",
+    help="If given, text in the speech bubble will not be wrapped"
+)
 parser.add_argument(
     "-E",
     type=str,
@@ -33,6 +53,7 @@ def get_max_str(a):
         if b=='\n' : m=m if m>=k else k; k=0
         k+=1
     return m
+
 def merge_cow(cow1, cow2): 
     new=''
     m=get_max_str(cow1); e=0; s=0;  
@@ -44,14 +65,15 @@ def merge_cow(cow1, cow2):
     return new
     
 
-cow1=cowsay.cowsay(sys.argv[-2])
-cow2=cowsay.cowsay(sys.argv[-1], F, eyes = E, wrap_text=N)
+cow1 = cowsay.cowsay(message=args.m1, eyes=args.eyes, tongue=args.tongue, cow=args.cowfile)
+cow2 = cowsay.cowsay(message=args.m2, eyes=args.E, tongue=args.N, cow=args.F)
+
 l1=cow1.count('\n')
 l2=cow2.count('\n') 
-if l1<l2: 
-    cow1='\n'*(l2-l1)+cow1
-elif l2<l1: 
-    cow2='\n'*(l2-l1)+cow2
+
+if l1<l2: cow1='\n'*(l2-l1)+cow1
+elif l2<l1: cow2='\n'*(l2-l1)+cow2
+
 print(merge_cow(cow1, cow2))
     
             
