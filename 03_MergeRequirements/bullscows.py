2cb4e697b71cc0671fc765523cd36fe8c50961af
Mary <maraihaidei03@gmail.com>
2025-03-01 02:30:22 +0300

    add function main, add changes in other functions, add function for reading dictionaries

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,7 @@
 import random 
+import sys 
+import urllib.request
+import typing
 
 def bullscows(d: str, z: str) -> (int, int): 
     cows = sum([1 for c in set(d) if c in set(z)])
@@ -17,11 +20,42 @@ def ask(promt, valid):
 def inform(format_string: str, bulls: int, cows: int) -> None: 
     print(format_string.format(bulls, cows))
 
-def gameplay(a: callable, inf: callable, words: list[str]) -> int: 
+def gameplay(a: typing.Callable[[str, typing.Optional[typing.List[str]]], str],
+             inf: typing.Callable[[str, int, int], None],
+             words: typing.List[str]) -> int:
     s = random.choice(words)
     asks=0
     while 1: 
         b, c = bullscows(a("Введите слово:", words), s); asks+=1
         inf("Быки: {}, Коровы: {}", b, c)
         if b == len(s): break; 
-    print(asks)
+    
+
+def read_dict(dit, l): 
+    if dit.startswith(('http://', 'https://')):
+        with urllib.request.urlopen(dit) as resp:
+            cont = resp.read().decode('utf-8')
+    else:
+        with open(dit, 'r', encoding='utf-8') as file:
+            cont = file.read()
+    ret = [word.strip() for word in cont.split() if word.strip()]  
+    return [r for r in ret if len(r)==l]
+
+def main(): 
+    lenth=5
+    if len(sys.argv)<2 :
+        print("Использование: python -m bullscows словарь длина")
+        return 
+    if len(sys.argv)>2 : 
+        try: 
+            lenth=int(sys.argv[-1])
+        except : 
+            print("Длина слова - натуральное число")
+    dit = read_dict(sys.argv[-2], lenth)
+    if not dit: 
+        print("В словаре нет слов длинны ", lenth)
+        return 
+    print('Вы угадали слово! Количество попыток:', gameload(ask, inform, lenth))
+    
+if __name__ == "__main__":
+    main() 
