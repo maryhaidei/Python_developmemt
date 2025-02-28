2600302f4944247bf3aa6d7b8639c8da5f57151f
Mary <maraihaidei03@gmail.com>
2025-03-01 01:34:34 +0300

    add function ask

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -3,4 +3,13 @@ def bullscows(d: str, z: str) -> (int, int):
     bulls = sum ([1 for i in range(len(d)) if d[i]==z[i]])
     return (bulls, cows-bulls)
 
-    
+def ask(promt, valid): 
+    print(promt)
+    if valid: 
+        st=input()
+        while st not in valid: 
+            print(promt + 'из списка:')
+            print(*valid, sep = ',' )
+            st=input()
+    else: st=input()
+    return st 
