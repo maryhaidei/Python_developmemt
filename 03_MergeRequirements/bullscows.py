bb33fa8c11f9b33a77df9575d3eb4ba1dd23f583
Mary <maraihaidei03@gmail.com>
2025-03-01 02:02:15 +0300

    add function gameplay, small changes in ask

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,5 @@
+import random 
+
 def bullscows(d: str, z: str) -> (int, int): 
     cows = sum([1 for c in set(d) if c in set(z)])
     bulls = sum ([1 for i in range(len(d)) if d[i]==z[i]])
@@ -5,15 +7,21 @@ def bullscows(d: str, z: str) -> (int, int):
 
 def ask(promt, valid): 
     print(promt)
-    if valid: 
-        print(*valid, sep = ',' )
-        st=input()
+    st=input()
+    if valid:
         while st not in valid: 
-            print(promt + 'из списка:')
-            print(*valid, sep = ',' )
+            print(promt)
             st=input()
-    else: st=input()
     return st 
 
 def inform(format_string: str, bulls: int, cows: int) -> None: 
     print(format_string.format(bulls, cows))
+
+def gameplay(a: callable, inf: callable, words: list[str]) -> int: 
+    s = random.choice(words)
+    asks=0
+    while 1: 
+        b, c = bullscows(a("Введите слово:", words), s); asks+=1
+        inf("Быки: {}, Коровы: {}", b, c)
+        if b == len(s): break; 
+    print(asks)
