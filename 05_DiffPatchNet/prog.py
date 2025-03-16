58054ec391c6f0bc7dcffc08ce28560fe36a980b
Mary <maraihaidei03@gmail.com>
2025-03-16 20:32:02 +0300

    add function Exec make an comand

--- 05_DiffPatchNet/prog.py
+++ 05_DiffPatchNet/prog.py
@@ -1,15 +1,47 @@
 import asyncio
 import cowsay
- 
+import shlex
+
 clients = {}
 
+async def Exec(cmd: str):
+       nonlocal buffer, login, stopped
+       match shlex.split(cmd):
+              case ["login", _login]:
+                     if (_login in cowsay.list_cows() and _login not in clients and buffer not in clients.values()):
+                            login = _login
+                            clients[login] = buffer
+                            await buffer.put("Login successful")
+                     else:
+                            await buffer.put("Login failed")
+              case ["who"]:
+                     await buffer.put('\n'.join(clients))
+              case ["cows"]:
+                     await buffer.put('\n'.join(n for n in cowsay.list_cows() if n not in clients))
+              case ["say", dst, msg]:
+                     if login in clients and dst in clients:
+                            await clients[dst].put(cowsay.cowsay(msg, cow=login))
+                     else:
+                            await buffer.put("Sending failed")
+              case ["yield", msg]:
+                     if login in clients:
+                            for dst in clients.values():
+                                   if dst is not buffer:
+                                          await dst.put(cowsay.cowsay(msg, cow=login))
+                                   else:
+                                          await buffer.put("Sending failed")
+              case ["quit"]:
+                     if login in clients:
+                            del clients[login]
+                            del buffer
+                            stopped = True
  
 async def cowChat(reader, writer):
-     pass
+       pass     
  
 async def main():
-     server = await asyncio.start_server(cowChat, "localhost", 1337)
-     async with server: 
-          await server.serve_forever()
+       server = await asyncio.start_server(cowChat, "localhost", 1337)
+       async with server: 
+              await server.serve_forever()
  
 asyncio.run(main())
